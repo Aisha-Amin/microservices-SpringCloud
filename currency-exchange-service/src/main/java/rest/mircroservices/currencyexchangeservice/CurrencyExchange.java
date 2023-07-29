ba3f2d83c94aedeea618ed624408ce9aa640c911
Aisha Amin <aisha.aminn@gmail.com>
2023-07-29 20:05:36 +0200

    Configure JPA and JPA Repository

--- currency-exchange-service/src/main/java/rest/mircroservices/currencyexchangeservice/CurrencyExchange.java
+++ currency-exchange-service/src/main/java/rest/mircroservices/currencyexchangeservice/CurrencyExchange.java
@@ -1,7 +1,13 @@
 package rest.mircroservices.currencyexchangeservice;
 
 
-import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 import java.math.BigDecimal;
 
@@ -9,11 +15,22 @@ import java.math.BigDecimal;
 @ToString
 @Getter
 @Setter
+@Entity
 public class CurrencyExchange {
+
+    @Id
     private Long id;
+
+    @Column(name="currency_from")
     private String from;
+
+    @Column(name="currency_to")
     private String to;
+
+    @Column(name="conversion_multiple")
     private BigDecimal conversionMultiple;
+
+    @Column(name="environment")
     private String environment;
 
     public CurrencyExchange(Long id, String from, String to, BigDecimal conversionMultiple) {
