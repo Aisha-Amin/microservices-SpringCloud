f08dadb701589e80aa2bac0c62b531f1cc4a336c
Aisha Amin <aisha.aminn@gmail.com>
2023-07-29 22:02:14 +0200

    Set up Eureka Naming Server

---
+++ naming-server/src/test/java/rest/mircroservices/namingserver/NamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package rest.mircroservices.namingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
