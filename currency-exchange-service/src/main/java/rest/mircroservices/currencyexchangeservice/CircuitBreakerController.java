6e3feef3b8b10a19501a1c52a31476563a007e40
Aisha Amin <aisha.aminn@gmail.com>
2023-07-30 13:50:41 +0200

    Circuit Breaker Resilience4j with Rate Limiter and BulkHead

--- currency-exchange-service/src/main/java/rest/mircroservices/currencyexchangeservice/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/rest/mircroservices/currencyexchangeservice/CircuitBreakerController.java
@@ -1,12 +1,10 @@
 package rest.mircroservices.currencyexchangeservice;
 
-import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CircuitBreakerController {
@@ -14,12 +12,18 @@ public class CircuitBreakerController {
     private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
 
     @GetMapping("/sample-api")
-    @Retry(name = "sample-api", fallbackMethod = "defaultResponse")
+   // @Retry(name = "sample-api", fallbackMethod = "defaultResponse")
+    //@CircuitBreaker(name = "default", fallbackMethod = "defaultResponse")
+    //@RateLimiter(name="default")
+    //10s -> 1000 calls to sample api
+    @Bulkhead(name ="sample-api")
     public String sampleApi(){
 
         logger.info("Sample Api call received");
-        ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url", String.class);
-        return forEntity.getBody();
+//        ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url", String.class);
+//        return forEntity.getBody();
+
+        return "sample-api";
     }
 
     // Since it should be throwable so we use Exception
