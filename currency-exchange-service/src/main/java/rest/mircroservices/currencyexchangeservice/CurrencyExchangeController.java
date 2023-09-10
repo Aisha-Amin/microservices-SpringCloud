169b27e09f460d6eaaeb80ccf638bc6f6820b47e
Aisha Amin <aisha.aminn@gmail.com>
2023-09-10 14:14:46 +0200

    Connecting microservices with Zipkin to identify traces and metrics

--- currency-exchange-service/src/main/java/rest/mircroservices/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/rest/mircroservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package rest.mircroservices.currencyexchangeservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CurrencyExchangeController {
 
+    private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
+
     @Autowired
     private Environment environment;
 
@@ -17,6 +21,8 @@ public class CurrencyExchangeController {
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange getExchangeValue(@PathVariable String from , @PathVariable String to){
+
+        logger.info("getExchangeValue called with {} to {} ", from,to);
         CurrencyExchange currencyExchange = currencyExchangeRepository.findByFromAndTo(from,to);
 
         if(currencyExchange == null)
